ce983dd07b344b1c09a0844957abcec8e424c43c
Kirill Denisov <kirilledition@protonmail.com>
2025-01-07 20:38:23 +0800

    67 current  2025-01-07 20:38:21  24.11.20250105.cbd8ec4  6.12.8                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -196,7 +196,7 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
   hardware.nvidia-container-toolkit.enable = true;
 
