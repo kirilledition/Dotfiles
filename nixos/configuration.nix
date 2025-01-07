bd0805a9fc3dd3d7246dc63dc9dab085abf10aae
Kirill Denisov <kirilledition@protonmail.com>
2025-01-07 22:45:03 +0800

    68 current  2025-01-07 22:45:01  24.11.20250105.cbd8ec4  6.12.8                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -250,12 +250,12 @@
     '';
   };
 
-  console = {
-    earlySetup = true;
-    font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
-    packages = with pkgs; [terminus_font];
-    keyMap = "us";
-  };
+  # console = {
+  #   earlySetup = true;
+  #   font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
+  #   packages = with pkgs; [terminus_font];
+  #   keyMap = "us";
+  # };
 
   catppuccin = {
     enable = true;
